d26146c791551ae6c36f513198676ecc1de3bc32
Mihai Pop <mihai.pop@fuzelabs.ro>
2024-08-25 20:21:49 +0300

    add rust analyzer in lsp integration

--- init.lua
+++ init.lua
@@ -566,7 +566,7 @@ require('lazy').setup({
         cmake = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
